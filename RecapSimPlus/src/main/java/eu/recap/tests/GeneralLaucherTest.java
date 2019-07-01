99fb89ae53a22af12789271412f64f949c04886e
jbernard-dcu <joseph.98@free.fr>
2019-07-01 12:31:39 +0100

    general launcher test

---
+++ RecapSimPlus/src/main/java/eu/recap/tests/GeneralLaucherTest.java
@@ -0,0 +1,25 @@
+package eu.recap.tests;
+
+public class GeneralLaucherTest {
+	
+	/* TODO
+	 * Define parameters
+	 */
+
+	
+	public static void main(String[] args) {
+		
+		/*TODO
+		 * Follow the main of Linknovate, adding tunable parameters
+		 */
+		
+		final int nSites=1;
+		final int nNodesPerSite=1;
+		
+		long startTime=System.currentTimeMillis();
+		
+		
+		
+	}
+
+}
